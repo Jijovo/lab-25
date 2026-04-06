27e7d9b7c29cfe9d6dc18d0524518ac09b800c94
Jijovo <jijovo77@gmail.com>
2026-04-05 20:32:34 -0700

    sort

--- main.cpp
+++ main.cpp
@@ -46,11 +46,44 @@ int main() {
     end = chrono::high_resolution_clock::now();
     duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
     stRead = duration.count();
-    //test print
+    fin.close();
+    /*test print
     cout << setw(10) << "Vector" << setw(10) << "List" << setw(10) << "Set" << endl;
     cout << setw(10) << vecRead << setw(10) << lstRead << setw(10) << stRead << endl;
+    */
+   //test sorting
+   //vector
+    int vecSort;
+    start = chrono::high_resolution_clock::now();
+    for(int i = 0; i < vec.size(); i++) {
+        for(int j = 0; j < vec.size() - 1; j++) {
+            if(vec[j] > vec[j + 1]) {
+                swap(vec[j], vec[j + 1]);
+            }
+        }
+    }
+    end = chrono::high_resolution_clock::now();
+    duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
+    vecSort = duration.count();
+    //list
+    int lstSort;
+    start = chrono::high_resolution_clock::now();
+    lst.sort();
+    end = chrono::high_resolution_clock::now();
+    duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
+    lstSort = duration.count();
+    //set
+    int stSort;
+    start = chrono::high_resolution_clock::now();
+    end = chrono::high_resolution_clock::now(); //already sorted
+    duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
+    stSort = duration.count();
+    //print results
+    cout << setw(10) << "Vector" << setw(10) << "List" << setw(10) << "Set" << endl;
+    cout << setw(10) << vecRead << setw(10) << lstRead << setw(10) << stRead << endl;
+    cout << setw(10) << vecSort << setw(10) << lstSort << setw(10) << stSort << endl;
+
     return 0;
-    
 }
 
 /* syntax examples:
